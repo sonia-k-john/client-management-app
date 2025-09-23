74ac1db7ec2f97ea5a7b80edb9f3284fe4afe25f
sonia-k-john <soniajohnhc@gmail.com>
2025-09-23 23:14:33 +0530

    Meeting Scheduler Component setup

--- src/app/meeting-scheduling/meeting-scheduling.component.ts
+++ src/app/meeting-scheduling/meeting-scheduling.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class MeetingSchedulingComponent {
 
+  meeting = {
+    topic: '',
+    numberOfPeople: 1,
+    date: '',
+    time: '',
+    duration: ''
+  };
+
+  meetingsList: any[] = [];
+
+  addMeeting() {
+    if (
+      this.meeting.topic &&
+      this.meeting.numberOfPeople &&
+      this.meeting.date &&
+      this.meeting.time &&
+      this.meeting.duration
+    ) {
+      this.meetingsList.push({ ...this.meeting });
+
+      this.meeting = { topic: '', numberOfPeople: 1, date: '', time: '', duration: '' };
+
+      alert(`Meeting ${this.meeting.topic} scheduled successfully!`);
+    } else {
+      alert('Please fill all fields.');
+    }
+  }
+
 }
