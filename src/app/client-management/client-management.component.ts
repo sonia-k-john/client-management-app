d5f3e4122401aa5bf23136962d8d4495bc19f47b
sonia-k-john <soniajohnhc@gmail.com>
2025-09-23 23:00:10 +0530

    Client Management Component setup

--- src/app/client-management/client-management.component.ts
+++ src/app/client-management/client-management.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class ClientManagementComponent {
 
+  client = {
+    name: '',
+    email: '',
+    address: '',
+    password: '',
+    repeatPassword: ''
+  };
+
+  clientsList: any[] = [];
+
+  addClient() {
+    if (
+      this.client.name &&
+      this.client.email &&
+      this.client.address &&
+      this.client.password &&
+      this.client.repeatPassword
+    ) {
+      if (this.client.password !== this.client.repeatPassword) {
+        alert('Passwords do not match!');
+        return;
+      }
+
+      this.clientsList.push({
+        name: this.client.name,
+        email: this.client.email,
+        address: this.client.address
+      });
+
+      // Reset the form
+      this.client = { name: '', email: '', address: '', password: '', repeatPassword: '' };
+
+      alert(`Client ${this.client.name} added successfully!`);
+    } else {
+      alert('Please fill all the client details.');
+    }
+  }
 }
